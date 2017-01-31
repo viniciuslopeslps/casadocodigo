d54b5921d662a935871ad28f10366c59d3f8da17
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-01-31 20:12:18 -0200

    arrumando problema com arquivos staticos

--- src/main/java/br/com/casadocodigo/shop/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/shop/conf/AppWebConfiguration.java
@@ -1,26 +1,27 @@
 	package br.com.casadocodigo.shop.conf;
-	
-	import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.format.datetime.DateFormatter;
-import org.springframework.format.datetime.DateFormatterRegistrar;
-import org.springframework.format.support.DefaultFormattingConversionService;
-import org.springframework.format.support.FormattingConversionService;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import br.com.casadocodigo.shop.controllers.HomeController;
-import br.com.casadocodigo.shop.daos.ProductDAO;
-import br.com.casadocodigo.shop.infra.FileSaver;
+	import br.com.casadocodigo.shop.controllers.HomeController;
+	import br.com.casadocodigo.shop.daos.ProductDAO;
+	import br.com.casadocodigo.shop.infra.FileSaver;
+	import org.springframework.context.MessageSource;
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.ComponentScan;
+	import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+	import org.springframework.format.datetime.DateFormatter;
+	import org.springframework.format.datetime.DateFormatterRegistrar;
+	import org.springframework.format.support.DefaultFormattingConversionService;
+	import org.springframework.format.support.FormattingConversionService;
+	import org.springframework.web.multipart.MultipartResolver;
+	import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+	import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+	import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+	import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+	import org.springframework.web.servlet.view.InternalResourceViewResolver;
 	
 	@EnableWebMvc
 	//pra cada package precisa colocar um novo desse aqui  (usado nas beans)
 	@ComponentScan(basePackageClasses = { HomeController.class, ProductDAO.class, FileSaver.class })
-	public class AppWebConfiguration {
+	public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
 		//confiugração para pasta de views
 		@Bean
@@ -55,5 +56,11 @@ import br.com.casadocodigo.shop.infra.FileSaver;
 		@Bean
 		public MultipartResolver multipartResolver(){
 			return new StandardServletMultipartResolver();
+		}
+
+		//
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 		}
 	}
--- src/main/webapp/WEB-INF/views/products/detail.jsp
+++ src/main/webapp/WEB-INF/views/products/detail.jsp
@@ -12,27 +12,27 @@
     <link rel="icon" href="//cdn.shopify.com/s/files/1/0155/7645/t/177/assets/favicon.ico?11981592617154272979" type="image/ico"/>
     <link href="https://plus.googlecom/108540024862647200608" rel="publisher"/>
     <title>${ product.title }-Casadocódigo</title>
-    <link href="${contextPath}/resources/css/cssbase-min.css"
+    <link href="${contextPath}resources/css/cssbase-min.css"
           rel="stylesheet" type="text/css" media="all"/>
     <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700'
           rel='stylesheet'/>
-    <link href="${contextPath}/resources/css/fonts.css" rel="stylesheet"
+    <link href="${contextPath}resources/css/fonts.css" rel="stylesheet"
           type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/fontello-ie7.css"
+    <link href="${contextPath}resources/css/fontello-ie7.css"
           rel="stylesheet" type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/fontello-embedded.css"
+    <link href="${contextPath}resources/css/fontello-embedded.css"
           rel="stylesheet" type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/fontello.css" rel="stylesheet"
+    <link href="${contextPath}resources/css/fontello.css" rel="stylesheet"
           type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/style.css" rel="stylesheet"
+    <link href="${contextPath}resources/css/style.css" rel="stylesheet"
           type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/layout-colors.css"
+    <link href="${contextPath}resources/css/layout-colors.css"
           rel="stylesheet" type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/responsive-style.css"
+    <link href="${contextPath}resources/cssresponsive-style.css"
           rel="stylesheet" type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/guia-do-programador-style.css"
+    <link href="${contextPath}resources/css/guia-do-programador-style.css"
           rel="stylesheet" type="text/css" media="all"/>
-    <link href="${contextPath}/resources/css/produtos.css" rel="stylesheet"
+    <link href="${contextPath}resources/css/produtos.css" rel="stylesheet"
           type="text/css" media="all"/>
     <link rel="canonical" href="http://www.casadocodigo.com.br/"/>
 </head>
