c67b9678c028c926fc643c003780409edc0d5e73
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-02-06 22:17:42 -0200

    home casa do codigo aula 1 parte 1

--- src/main/java/br/com/casadocodigo/shop/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/shop/controllers/HomeController.java
@@ -1,14 +1,25 @@
 package br.com.casadocodigo.shop.controllers;
 
+import br.com.casadocodigo.shop.daos.ProductDAO;
+import br.com.casadocodigo.shop.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/")
-	public String index() {
-		System.out.println("Entrando na home da CDC");
-		return "home.jsp";
-	}
+    @Autowired
+    private ProductDAO productDAO;
+
+    @RequestMapping("/")
+    public ModelAndView index(Model model) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        List<Product> products = productDAO.list();
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
 }
