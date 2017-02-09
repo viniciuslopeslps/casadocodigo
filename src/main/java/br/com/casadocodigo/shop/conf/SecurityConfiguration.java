00628d149c49c610d6ec369965c91295da07decf
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-02-09 21:44:04 -0200

    aula 2 spring security

--- src/main/java/br/com/casadocodigo/shop/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/shop/conf/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.shop.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,4 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    //CONFIGURA AS PERMISSOES DE ACORDO COM AS ROLES DOS USUARIOS APARTIR DA URL
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/produtos/form").hasRole("ADMIN")
+                .antMatchers("/carrinho").permitAll()
+                .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/produtos").permitAll()
+                .antMatchers("/produtos/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/resources/**").permitAll()
+                .anyRequest().authenticated().and().formLogin();
+    }
 }
